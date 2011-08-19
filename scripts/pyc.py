707566a8915b12d676cef6f5b11e0a48dc1bca8e
Brett Graham <graham@rowland.harvard.edu>
2011-08-19 15:09:19 -0400

    fixed bug: swapped tree/cdata in pyc

--- scripts/pyc.py
+++ scripts/pyc.py
@@ -226,7 +226,7 @@ else:
     spikefeatures = waveletfeatures.wavelet_features(spikewaveforms, nfeatures = options.nfeatures)
     
     # cluster
-    clusters, tree, cdata = cluster.spc(spikefeatures, quiet = (not options.verbose), nclusters = options.nclusters)
+    clusters, cdata, tree = cluster.spc(spikefeatures, quiet = (not options.verbose), nclusters = options.nclusters)
     logging.debug("Found Clusters: %s" % (str([len(c) for c in clusters])))
     clusterindices = cluster.clusters_to_indices(clusters)
     
