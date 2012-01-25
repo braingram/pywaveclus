a7403187d9984c0747ec7a2c6b6ac9e8ccb99ce4
Brett Graham <graham@rowland.harvard.edu>
2012-01-25 11:48:57 -0500

    adding pca.stack_waveforms test

---
+++ pywaveclus/dsp/tests/test_pca.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+import pywaveclus
+
+def test_stack_waveforms():
+    # make 2d waveforms
+    x = 13
+    y = 7
+    z = 5
+
+    w2d = np.arange(x*y).reshape((x,y))
+    w3d = np.arange(x*y*z).reshape((x,y,z))
+
+    f = pywaveclus.dsp.pca.stack_waveforms # i'm lazy
+    
+    # check 2d
+    assert(w2d.shape == f(w2d).shape) # check shapes
+    assert(all(f(w2d) == w2d)) # check values
+
+    # check 3d
+    t3d = np.arange(x*y*z).reshape((x,y*z))
+    assert(t3d.shape == f(w3d).shape) # check shapes
+    assert(all(t3d == f(w3d))) # check values
