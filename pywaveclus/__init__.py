fa73a7e5e8a9db4dd357813194665b7a8bcea866
Brett Graham <graham@rowland.harvard.edu>
2012-02-07 11:26:06 -0500

    upping version number

--- pywaveclus/__init__.py
+++ pywaveclus/__init__.py
@@ -3,6 +3,6 @@
 # import cluster, detect, waveletfeatures, waveletfilter
 import cluster, data, detect, dsp, extract, process, template, utils
 
-__version__ = '1.0'
+__version__ = '1.0.1'
 
 __all__ = ['cluster', 'data', 'detect', 'dsp', 'extract', 'process', 'template', 'utils']
