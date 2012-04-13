87a6c895ec59b87f9092166558a8eab520d22a63
Brett Graham <graham@rowland.harvard.edu>
2012-04-13 11:22:01 -0400

    pep8

--- scripts/pycluster.py
+++ scripts/pycluster.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-import logging, os, sys
+import logging
+import os
+import sys
 
 import numpy as np
 import pylab as pl
@@ -15,37 +17,41 @@ cfg, times, waves, clusters, info = pywaveclus.process.process.process_file()
 
 logging.debug("%i spikes before saving" % len(times))
 
-# ------------------------------------- move this into process_file at some point
+# ----- move this into process_file at some point
 
 # get output directory (assign default if missing)
-outdir = cfg.get('main','outputdir').strip()
-filename = cfg.get('main','filename')
-if outdir == '': outdir = os.path.dirname(os.path.abspath(filename)) + '/pyc_' + os.path.basename(filename)
-# 
+outdir = cfg.get('main', 'outputdir').strip()
+filename = cfg.get('main', 'filename')
+if outdir == '':
+    outdir = os.path.dirname(os.path.abspath(filename)) + \
+            '/pyc_' + os.path.basename(filename)
+#
 # if not os.path.exists(outdir): os.makedirs(outdir)
 # logging.root.addHandler(logging.FileHandler('%s/pyc.log' % outdir, mode='w'))
 
-outfile = '/'.join((outdir, os.path.splitext(os.path.basename(filename))[0])) + '.h5'
+outfile = '/'.join((outdir, os.path.splitext(os.path.basename(filename))[0])) \
+        + '.h5'
 logging.debug("Saving results to %s" % outfile)
 
-pre = cfg.getint('detect','pre')
-post = cfg.getint('detect','post')
-nadj = len(cfg.get('main','adjacentfiles').split())
+pre = cfg.getint('detect', 'pre')
+post = cfg.getint('detect', 'post')
+nadj = len(cfg.get('main', 'adjacentfiles').split())
+
 
 class description(tables.IsDescription):
     time = tables.Int64Col()
     wave = tables.Float64Col(shape=(pre + post))
     clu = tables.UInt8Col()
 
-hdfFile = tables.openFile(outfile,"w")
-spiketable = hdfFile.createTable("/","SpikeTable",description)
+hdfFile = tables.openFile(outfile, "w")
+spiketable = hdfFile.createTable("/", "SpikeTable", description)
 
-minref = 22 # 0.0005 s at 44100 Hz
+minref = 22  # 0.0005 s at 44100 Hz
 
 logging.debug("writing to hdf5 file")
-for i in xrange(len(times)-1):
-    nspike = np.where(clusters[i+1:] == clusters[i])[0]
-    if (len(nspike) > 0) and (times[nspike[0]+i+1] - times[i] < minref):
+for i in xrange(len(times) - 1):
+    nspike = np.where(clusters[i + 1:] == clusters[i])[0]
+    if (len(nspike) > 0) and (times[nspike[0] + i + 1] - times[i] < minref):
         # caton skips spikes that are the same cluster and
         # occur close together in time
         logging.debug("Throwing away spike at %i" % times[i])
@@ -85,7 +91,7 @@ else:
     clusters = np.array(clusters)
 
 nclusters = np.max(clusters) + 1
-colors = matplotlib.cm.get_cmap('jet',nclusters)
+colors = matplotlib.cm.get_cmap('jet', nclusters)
 
 
 pl.figure(1)
@@ -93,13 +99,13 @@ pl.suptitle('Spike Times')
 pl.xlabel('Time(samples)')
 pl.ylabel('Amplitude')
 
-n_features = cfg.getint('cluster','nfeatures')
+n_features = cfg.getint('cluster', 'nfeatures')
 
-pl.figure(2, figsize=(nclusters*3,n_features))
+pl.figure(2, figsize=(nclusters * 3, n_features))
 pl.suptitle("Spike Waveforms")
 pl.ylabel("Amplitude")
 
-pl.figure(3, figsize=(n_features,n_features))
+pl.figure(3, figsize=(n_features, n_features))
 pl.suptitle("Spike Features")
 
 logging.debug("Waveform array shape: %s" % str(waves.shape))
@@ -110,36 +116,41 @@ logging.debug("Features array shape: %s" % str(features.shape))
 
 for cl in xrange(nclusters):
     gi = np.where(clusters == cl)
-    if (len(gi) == 0): continue
+    if (len(gi) == 0):
+        continue
     gi = gi[0]
-    if (len(gi) == 0): continue
+    if (len(gi) == 0):
+        continue
     st = times[gi]
-    sw = waves[gi,0,:] # only plot first wave (from main channel)
-    se = sw[:,pre]
+    sw = waves[gi, 0, :]  # only plot first wave (from main channel)
+    se = sw[:, pre]
     c = colors(cl)
     # plot times
     pl.figure(1)
-    pl.scatter(st,se,label='%i' % cl, color=c)
-    
-    for i in xrange(nadj+1):
-        sw = waves[gi,i,:]
+    pl.scatter(st, se, label='%i' % cl, color=c)
+
+    for i in xrange(nadj + 1):
+        sw = waves[gi, i, :]
         # plot waves
         pl.figure(2)
-        pl.subplot((nadj+1)*2,nclusters,cl+1+nclusters*(i*2))
+        pl.subplot((nadj + 1) * 2, nclusters, cl + 1 + nclusters * (i * 2))
         pl.plot(sw.T, color=c)
         pl.title("N=%i" % len(sw))
-        pl.subplot((nadj+1)*2,nclusters,cl+nclusters+1+nclusters*(i*2))
-        av = np.average(sw,0)
-        sd = np.std(sw,0,ddof=1)
+        pl.subplot((nadj + 1) * 2, nclusters, cl + nclusters + 1 + \
+                nclusters * (i * 2))
+        av = np.average(sw, 0)
+        sd = np.std(sw, 0, ddof=1)
         se = sd / np.sqrt(len(sw))
-        pl.fill_between(range(len(av)), av+se*1.96, av-se*1.96, color=c, alpha=0.5)
+        pl.fill_between(range(len(av)), av + se * 1.96, av - se * 1.96, \
+                color=c, alpha=0.5)
         pl.plot(av, color=c)
-    
+
     # plot features
     pl.figure(3)
-    sf = features[gi,:]
+    sf = features[gi, :]
     ndims = int(sf.shape[1])
-    logging.debug("Feature Dims: %i, %s, %s" % (ndims, str(sf.shape), str(type(features))))
+    logging.debug("Feature Dims: %i, %s, %s" % \
+            (ndims, str(sf.shape), str(type(features))))
     for x in xrange(ndims):
         for y in xrange(ndims):
             if y < x:
@@ -150,11 +161,12 @@ for cl in xrange(nclusters):
                 #sys.stderr.flush()
                 #pl.subplot((ndims, ndims, x + y * ndims + 1))
                 #pl.subplot(*spt)
-                pl.scatter(sf[:,x], sf[:,y], s = 1, label='%i', color=c, alpha = 0.5)
+                pl.scatter(sf[:, x], sf[:, y], s=1, label='%i', color=c, \
+                        alpha=0.5)
                 pl.xticks([])
                 pl.yticks([])
 
 logging.debug("Saving plots")
-for (i,name) in enumerate(['times','waves','features']):
-    pl.figure(i+1)
+for (i, name) in enumerate(['times', 'waves', 'features']):
+    pl.figure(i + 1)
     pl.savefig('/'.join((outdir, name + '.png')))
