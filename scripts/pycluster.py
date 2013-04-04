f4323a54b8d907e32c0222e1b4dd65e53d9867c1
Brett Graham <brettgraham@gmail.com>
2013-04-04 18:12:34 -0400

    na

--- scripts/pycluster.py
+++ scripts/pycluster.py
@@ -25,6 +25,9 @@ if len(sys.argv) > 2:
 # get afs
 afdir = bdir + '/Audio Files'
 assert os.path.exists(afdir)
+fns = sorted([os.path.realpath(fn) for fn in glob.glob(afdir + '/input*')])
+print "filenames:", fns
+
 ica = None
 ica_mm = bdir + '/mixingmatrix'
 ica_um = bdir + '/unmixingmatrix'
@@ -32,13 +35,10 @@ if (os.path.exists(ica_mm) and os.path.exists(ica_um)):
     mm = numpy.matrix(numpy.loadtxt(ica_mm))
     um = numpy.matrix(numpy.loadtxt(ica_um))
     cm = mm * um
-    ica = dict(cm=cm)
+    ica = dict(cm=cm, fns=fns)
     print "found ica"
 else:
     print "no ica found"
-
-fns = sorted([os.path.realpath(fn) for fn in glob.glob(afdir + '/input*')])
-print "filenames:", fns
 
 store = pywaveclus.ops.store.hdf5.SpikeStorage(
     tables.openFile(ofn, 'w'))
